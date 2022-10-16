f7e0d9558b938e0f8f36454bbfde51fa951e5d54
Agathius <p.akos1148@gmail.com>
2022-10-16 18:18:26 +0200

    testing out routing params (single and multiple params)

---
+++ 02-express-tutorial/07-routing-params.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { products } = require('./data');
+
+const app = express();
+  app.get('/', (req, res)=> { 
+    res.send(
+      `<H1> Home Page </H1> 
+      <p> <a href='./api/products'> Click here </a> to view products </p>`
+    )
+  })
+
+  //Route Params
+  // 1. Single param (dynamic way) 
+  app.get('/api/products/:ProductID', (req, res) => { // defining a custom route parameter 
+    const { ProductID } = req.params; //get the route parameter 
+
+    const filteredProduct = products.filter((product) => product.id === Number(ProductID)) // returns obj if its id === ProductID
+    res.json(filteredProduct); //send back  json
+  });
+
+  // 2. Multiple params (dynamic way)  (hardcoded version)
+  // defining custom route parameters {UserID, BookName}
+  app.get('/api/users/:UserID/book/:BookName', (req, res) => {
+    // get params
+    const { UserID, BookName } = req.params; 
+    console.log(req.params);
+	//send back html with the requested user id and book name
+	res.send(`
+		<H1> Request result: </H1>
+		<p> User name: ${UserID} </p>	
+		<p> Found book: ${BookName} </p>	
+  `); 
+});
+
+app.listen(5000, () => {console.log('listening on port 5000')})
