f2d5942a2bbeec1be3558bee5dc3532124cc1011
Agathius <p.akos1148@gmail.com>
2022-10-16 15:03:06 +0200

    testing out express.static() and res.sendFile()

---
+++ 02-express-tutorial/04-express-app-static.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+//initialize server
+const app = express();
+
+//set up middleware 
+app.use(express.static('./public')); //you can serve all the needed files (e.g. .html .css, .svg, .js, etc.) using the static middleware
+
+//alternatively you can serve files using app.get -> res.sendFile (here we serve index.html) 
+/*
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+})
+*/
+
+app.get('*', (req, res) => {
+  res.status(404).send('resource is not available.')
+})
+
+app.listen(5000, ()=> {
+  console.log('server is listening on port 5000...')
+})
