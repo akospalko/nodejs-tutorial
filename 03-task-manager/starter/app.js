a5869bee355dc083ce9a6329a430cc8244f79ff3
Agathius <p.akos1148@gmail.com>
2022-12-11 23:57:03 +0100

    added error handling, reused task controllers code,  finished project.

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,17 +3,22 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config(); // access .env contents
-const port = 3000;
+const notFound = require('./middleware/not-found.js'); 
+const errorHandlerMiddleware = require('./middleware/error-handler.js'); 
 
+const port = 3000;
 //middleware
 app.use(express.json());  //built in middleware function in Express . It parses incoming JSON requests and puts the parsed data in req.body.
+app.use(express.static('./public'));
+app.use(errorHandlerMiddleware);
 
+//routes
 app.get('/hello', (req, res) => {
   res.send('Task Manager App');
 });
 
 app.use('/api/v1/tasks', tasks);
-
+app.use(notFound); // if route is not found (it is matched if )
 
 const startServer = async () => {
   try {
